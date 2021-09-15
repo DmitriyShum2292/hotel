4730266fd555e491407d86650551fdd8c4dec673
Dmitriy <dmitriyshum2292@gmail.com>
2021-09-15 13:18:37 +0500

    Added NotFoundException class and Exception Handler method

--- src/main/java/com/example/hotels/exception/ApiExceptionHandler.java
+++ src/main/java/com/example/hotels/exception/ApiExceptionHandler.java
@@ -2,23 +2,20 @@ package com.example.hotels.exception;
 
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class ApiExceptionHandler {
 
-    @ExceptionHandler(value = {NullPointerException.class})
-    public ModelAndView notFoundException(NullPointerException e){
+    @ExceptionHandler(value = {NotFoundException.class})
+    public ModelAndView notFoundException(NotFoundException e){
         ModelAndView mav = new ModelAndView();
         mav.setStatus(HttpStatus.NOT_FOUND);
         mav.setViewName("exception");
         mav.addObject("status", HttpStatus.NOT_FOUND.toString());
-        mav.addObject("exception", e.toString());
+        mav.addObject("exception", e.getMessage());
         return mav;
     }
 }
