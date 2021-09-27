505d9320c4fff4dbce6e90342121bdda47663ce5
Dmitriy <dmitriyshum2292@gmail.com>
2021-09-27 10:13:41 +0500

    added Notification.class

--- src/main/java/com/example/hotels/dto/Notification.java
+++ src/main/java/com/example/hotels/dto/Notification.java
@@ -1,4 +1,33 @@
 package com.example.hotels.dto;
 
+import java.time.LocalTime;
+
 public class Notification {
+
+    private LocalTime cleaningTime;
+    private String description;
+
+    public Notification() {
+    }
+
+    public Notification(LocalTime cleaningTime, String description) {
+        this.cleaningTime = cleaningTime;
+        this.description = description;
+    }
+
+    public LocalTime getCleaningTime() {
+        return cleaningTime;
+    }
+
+    public void setCleaningTime(LocalTime cleaningTime) {
+        this.cleaningTime = cleaningTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
