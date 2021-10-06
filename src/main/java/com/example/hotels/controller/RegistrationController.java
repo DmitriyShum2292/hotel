32451238360527852f7a62556650915f04ef314d
Dmitriy <dmitriyshum2292@gmail.com>
2021-10-06 14:25:27 +0500

    added @Valid to parameter user,added parameter binding request and model to register method for validation from registration form

--- src/main/java/com/example/hotels/controller/RegistrationController.java
+++ src/main/java/com/example/hotels/controller/RegistrationController.java
@@ -6,7 +6,10 @@ import com.example.hotels.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/registration")
@@ -22,7 +25,11 @@ public class RegistrationController {
     }
 
     @PostMapping
-    public String register(@ModelAttribute("user") User user){
+    public String register(@ModelAttribute("user") @Valid User user, BindingResult bindingResult,Model model){
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("error",bindingResult.getFieldErrors());
+            return "registration";
+        }
         User userExist = userService.findByUserId(user.getId());
         if (userExist!=null){
         return "redirect:/cabinet";
