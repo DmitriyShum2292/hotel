b25f531c001e16ee1a363ea575c740d3fe48a6e5
Dmitriy <dmitriyshum2292@gmail.com>
2021-10-12 13:33:39 +0500

    Get all district

--- src/main/java/com/example/hotels/service/HotelService.java
+++ src/main/java/com/example/hotels/service/HotelService.java
@@ -1,9 +1,11 @@
 package com.example.hotels.service;
 
+import com.example.hotels.dto.DistrictDTO;
 import com.example.hotels.model.Hotel;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.util.List;
 
 @Component
@@ -14,4 +16,5 @@ public interface HotelService {
     List<Hotel> findAllByNameStartingWithIgnoreCase(String name);
     Page<Hotel> findAll(int page, int size,String sort);
     void delete(long id);
+    List<DistrictDTO> getAllDistricts() throws IOException;
 }
