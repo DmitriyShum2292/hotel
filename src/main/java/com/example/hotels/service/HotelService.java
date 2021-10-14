df4f35b9e3ce5f99aa9ff24b194c28217b7fa2bc
Dmitriy <dmitriyshum2292@gmail.com>
2021-10-14 09:18:32 +0500

    Added getAllDistricts , getAllStreetsByDistrict , getCommercialBuildingsByStreet , getAllBuildingInfoByCommercialBuilding, saveLegalEntityInCityManagement methods. Added values from external.properties

--- src/main/java/com/example/hotels/service/HotelService.java
+++ src/main/java/com/example/hotels/service/HotelService.java
@@ -1,6 +1,9 @@
 package com.example.hotels.service;
 
+import com.example.hotels.dto.BuildingInfoDTO;
+import com.example.hotels.dto.CommercialBuildingDTO;
 import com.example.hotels.dto.DistrictDTO;
+import com.example.hotels.dto.StreetDTO;
 import com.example.hotels.model.Hotel;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
@@ -10,11 +13,17 @@ import java.util.List;
 
 @Component
 public interface HotelService {
-    void save(Hotel hotel);
+    void save(Hotel hotel) throws IOException;
+    void update(Hotel hotel);
     Hotel findById(long id);
     Hotel findByName(String name);
     List<Hotel> findAllByNameStartingWithIgnoreCase(String name);
     Page<Hotel> findAll(int page, int size,String sort);
     void delete(long id);
     List<DistrictDTO> getAllDistricts() throws IOException;
+    List<StreetDTO> getAllStreetsByDistrict(long districtId) throws IOException;
+    List<CommercialBuildingDTO> getAllCommercialBuildingByStreet(long streetId) throws IOException;
+    List<BuildingInfoDTO> getAllBuildingInfoByCommercialBuilding(long commercialBuildingId) throws IOException;
+    boolean saveLegalEntityInCityManagement(long homeId) throws IOException;
+
 }
