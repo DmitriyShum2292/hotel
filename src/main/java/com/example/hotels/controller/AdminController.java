0fd1764864cb2d9786726e4b2667fc35e7528a4b
Dmitriy <dmitriyshum2292@gmail.com>
2021-10-14 09:15:11 +0500

    Changed method createHotelPage,added params,and return pages

--- src/main/java/com/example/hotels/controller/AdminController.java
+++ src/main/java/com/example/hotels/controller/AdminController.java
@@ -1,17 +1,25 @@
 package com.example.hotels.controller;
 
+import com.example.hotels.dto.BuildingInfoDTO;
+import com.example.hotels.dto.CommercialBuildingDTO;
+import com.example.hotels.dto.DistrictDTO;
+import com.example.hotels.dto.StreetDTO;
+import com.example.hotels.exception.NotFoundException;
 import com.example.hotels.model.Hotel;
 import com.example.hotels.model.User;
 import com.example.hotels.service.HotelService;
 import com.example.hotels.service.OrderService;
 import com.example.hotels.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
+import java.io.IOException;
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
@@ -24,6 +32,9 @@ public class AdminController {
     @Autowired
     private OrderService orderService;
 
+    public Logger logger = LoggerFactory.getLogger(MainController.class);
+
+
     private final  String hotelName = "hotel";
     private final String redirectAdmin = "redirect:/admin";
 
@@ -35,15 +46,58 @@ public class AdminController {
         return "admin_cabinet";
     }
     @GetMapping("/hotel")
-    public String createHotelPage(Model model){
+    public String createHotelPage(@RequestParam(required = false) Long districtId,
+                                  @RequestParam(required = false) Long streetId,
+                                  @RequestParam(required = false)Long buildingId,
+                                  @RequestParam(required = false)Long homeId, Model model) throws IOException {
         User user = userService.findCurrentUser();
-        Hotel hotel = new Hotel();
         model.addAttribute("user", user);
+        if (districtId==null&&streetId==null&&buildingId==null){
+            List<DistrictDTO> districtDTOS = hotelService.getAllDistricts();
+            if (districtDTOS.isEmpty()){
+                throw new NotFoundException("Districts not found");
+            }
+            model.addAttribute("districts",districtDTOS);
+            return "district";
+        }
+        if(districtId!=null){
+            List<StreetDTO> streetDTOS = hotelService.getAllStreetsByDistrict(districtId);
+            if (streetDTOS.isEmpty()){
+                throw new NotFoundException("Streets not found in this district");
+            }
+            model.addAttribute("streets",streetDTOS);
+            return "street";
+        }
+        if (streetId!=null){
+            List<CommercialBuildingDTO> commercialBuildingDTOS = hotelService.getAllCommercialBuildingByStreet(streetId);
+            if (commercialBuildingDTOS.isEmpty()){
+                throw new NotFoundException("Commercial buildings not found on this street");
+            }
+            model.addAttribute("buildings",commercialBuildingDTOS);
+            return "building";
+        }
+        if (buildingId!=null){
+            List<BuildingInfoDTO> buildingInfoDTOS = hotelService.getAllBuildingInfoByCommercialBuilding(buildingId);
+            if (buildingInfoDTOS.isEmpty()){
+                throw new NotFoundException("You can't create a hotel here!");
+            }
+            model.addAttribute("homeId",buildingInfoDTOS.get(0).getId());
+
+            Hotel hotel = new Hotel();
+            model.addAttribute("user", user);
+            model.addAttribute(hotelName, hotel);
+
+            return "create_hotel";
+        }
+        Hotel hotel = new Hotel();
+
         model.addAttribute(hotelName, hotel);
+
         return "create_hotel";
     }
     @PostMapping("/hotel")
-    public String createHotel(@ModelAttribute("hotel") @Valid Hotel hotel, BindingResult bindingResult, Model model){
+    public String createHotel(@ModelAttribute("hotel") @Valid Hotel hotel, BindingResult bindingResult,
+                              Model model) throws IOException {
         if (bindingResult.hasErrors()) {
             model.addAttribute("error",bindingResult.getFieldErrors());
             User user = userService.findCurrentUser();
@@ -65,7 +119,7 @@ public class AdminController {
     }
     @PostMapping("/hotel/update")
     public String updateHotel(@ModelAttribute("hotel")Hotel hotel){
-        hotelService.save(hotel);
+        hotelService.update(hotel);
         return redirectAdmin;
     }
     @DeleteMapping("/hotel/{id}")
