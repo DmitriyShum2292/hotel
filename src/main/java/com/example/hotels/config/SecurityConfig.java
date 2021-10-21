8effabc3d516b3edcdebebb307e0502d83fbb23f
Dmitriy <dmitriyshum2292@gmail.com>
2021-10-21 13:02:35 +0500

    disable csrf for external post requests

--- src/main/java/com/example/hotels/config/SecurityConfig.java
+++ src/main/java/com/example/hotels/config/SecurityConfig.java
@@ -1,14 +1,11 @@
 package com.example.hotels.config;
 
-import com.example.hotels.hmac.HMACAuthFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.session.ConcurrentSessionFilter;
 
 import javax.sql.DataSource;
 
