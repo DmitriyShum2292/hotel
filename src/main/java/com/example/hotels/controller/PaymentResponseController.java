570f525cee072a5332c0b8d3a2cc2af616fbd14b
Dmitriy <dmitriyshum2292@gmail.com>
2021-10-29 09:06:44 +0500

    throws exception in setPaid method

--- src/main/java/com/example/hotels/controller/PaymentResponseController.java
+++ src/main/java/com/example/hotels/controller/PaymentResponseController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/api/v1/payment")
 public class PaymentResponseController {
@@ -21,7 +23,7 @@ public class PaymentResponseController {
      * receive response after payment is done in citizen account module
      */
     @PostMapping
-    public ResponseEntity<?> setPaid(@RequestBody CompleteRequestDTO completeRequestDTO){
+    public ResponseEntity<?> setPaid(@RequestBody CompleteRequestDTO completeRequestDTO) throws IOException {
         orderService.setPaid(completeRequestDTO);
         return new ResponseEntity<>(completeRequestDTO,HttpStatus.CREATED);
     }
