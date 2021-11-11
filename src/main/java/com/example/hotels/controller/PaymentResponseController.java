97dce7c6fca64dd20dc4bf6ebaf38de16e6c924f
Dmitriy <dmitriyshum2292@gmail.com>
2021-11-11 15:04:31 +0500

    changed return object

--- src/main/java/com/example/hotels/controller/PaymentResponseController.java
+++ src/main/java/com/example/hotels/controller/PaymentResponseController.java
@@ -1,8 +1,11 @@
 package com.example.hotels.controller;
 
+import com.example.hotels.dto.ApiResponse;
 import com.example.hotels.dto.CompleteRequestDTO;
 import com.example.hotels.service.OrderService;
 import com.example.hotels.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,13 +21,16 @@ public class PaymentResponseController {
     private OrderService orderService;
     @Autowired
     private UserService userService;
+    public Logger logger = LoggerFactory.getLogger(PaymentResponseController.class);
 
     /**
      * receive response after payment is done in citizen account module
      */
     @PostMapping
     public ResponseEntity<?> setPaid(@RequestBody CompleteRequestDTO completeRequestDTO) throws IOException {
+        logger.info("******************"+completeRequestDTO);
         orderService.setPaid(completeRequestDTO);
-        return new ResponseEntity<>(completeRequestDTO,HttpStatus.CREATED);
+
+        return new ResponseEntity<>(new ApiResponse(true,"true",completeRequestDTO),HttpStatus.OK);
     }
 }
