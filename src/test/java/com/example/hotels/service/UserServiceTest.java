89f3982021ac55995a8dabaa3d5519cfdaef4614
Dmitriy <dmitriyshum2292@gmail.com>
2021-11-24 11:48:17 +0500

    new methods and fixed imports

--- src/test/java/com/example/hotels/service/UserServiceTest.java
+++ src/test/java/com/example/hotels/service/UserServiceTest.java
@@ -1,20 +1,38 @@
 package com.example.hotels.service;
 
+import com.example.hotels.dto.ResidentReponseDTO;
+import com.example.hotels.hmac.HMACUtil;
+import com.example.hotels.model.ExternalApiCredentials;
 import com.example.hotels.model.Order;
 import com.example.hotels.model.User;
 import com.example.hotels.repository.UserRepository;
-import org.junit.jupiter.api.BeforeEach;
+import com.google.gson.Gson;
+import okhttp3.HttpUrl;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-
+import org.springframework.context.annotation.PropertySource;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.Date;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@PropertySource("classpath:external.properties")
 class UserServiceTest {
 
     @Autowired
@@ -22,13 +40,27 @@ class UserServiceTest {
     @MockBean
     private UserRepository userRepository;
 
-    private User user;
+    @Autowired
+    private HMACUtil hmacUtil;
+    @Autowired
+    private ExternalApiService externalApiService;
 
-    @BeforeEach
-    public void init(){
+    @Value("${city.management.baseurl}")
+    private String BASE_URL;
+    @Value("${city.management.resident.card.mapping}")
+    private String RESIDENT_CARD_URL;
+    @Value("${hotels.keyid}")
+    private String keyId;
+    @Value("${city.management.action.get}")
+    private String action;
+
+    private static User user;
+
+    @BeforeAll
+    public static void init(){
         user = new User();
         user.setId(1);
-        user.setUserId(1);
+        user.setUserId(14876580);
         user.setNickName("Login");
         user.setOrders(Arrays.asList(new Order()));
     }
@@ -39,15 +71,40 @@ class UserServiceTest {
     }
 
     @Test
+    void citizenExist() throws IOException, JSONException {
+        long now = new Date().getTime()+30000000;
+        String timestamp = String.valueOf(now);
+        ExternalApiCredentials externalApiCredentials = externalApiService.findByKeyId(keyId);
+        String signature = hmacUtil.calculateHash(keyId,timestamp,action,externalApiCredentials.getValue());
+
+        HttpUrl.Builder urlBuilder
+                = HttpUrl.parse(BASE_URL + RESIDENT_CARD_URL+user.getUserId()).newBuilder();
+        String url = urlBuilder.build().toString();
+
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        HttpGet request = new HttpGet(url);
+        request.addHeader(HttpHeaders.USER_AGENT, "Googlebot");
+        request.addHeader("sm-keyid",keyId);
+        request.addHeader("sm-timestamp",timestamp);
+        request.addHeader("sm-action",action);
+        request.addHeader("sm-signature",signature);
+        CloseableHttpResponse response = httpClient.execute(request);
+        HttpEntity entity = response.getEntity();
+
+        JSONObject obj = new JSONObject(EntityUtils.toString(entity));
+        Gson gson = new Gson();
+        ResidentReponseDTO residentReponseDTO = gson.fromJson(obj.getJSONObject("result").toString(),ResidentReponseDTO.class);
+
+        assertThat(response.getStatusLine().getStatusCode()==200);
+        assertThat(residentReponseDTO.isActive());
+    }
+
+    @Test
     void findByLogin() {
         when(userRepository.findByNickName(user.getNickName())).thenReturn(user);
         User testUser = userService.findByNickName(user.getNickName());
         assertThat(testUser.getNickName().equals(user.getNickName()));
-    }
-
-    @Test
-    void findCurrentUser() {
-        // don't know hot testing this method
     }
 
     @Test
