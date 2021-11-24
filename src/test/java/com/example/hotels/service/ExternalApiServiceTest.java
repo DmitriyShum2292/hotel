6458ab41e293f1140e74870d58e0985c43a3d1f8
Dmitriy <dmitriyshum2292@gmail.com>
2021-11-24 11:46:57 +0500

    Added ExternalApiService tests

---
+++ src/test/java/com/example/hotels/service/ExternalApiServiceTest.java
@@ -0,0 +1,27 @@
+package com.example.hotels.service;
+
+import com.example.hotels.model.ExternalApiCredentials;
+import com.example.hotels.repository.ExternalApiRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@SpringBootTest
+class ExternalApiServiceTest {
+
+    @Autowired
+    private ExternalApiService externalApiService;
+    @Mock
+    ExternalApiRepository externalApiRepository;
+    private static final String test = "test";
+
+    @Test
+    void findByKeyId() {
+        given(externalApiRepository.findByKeyId("test"))
+                .willReturn(new ExternalApiCredentials(1,test,test));
+        assertThat(externalApiRepository.findByKeyId(test).getValue().equals(test));
+    }
+}
