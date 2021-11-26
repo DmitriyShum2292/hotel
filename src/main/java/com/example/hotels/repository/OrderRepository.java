3482110cb68d904bf9122f9e70a9440412229245
Dmitriy <dmitriyshum2292@gmail.com>
2021-11-26 11:02:01 +0500

    new methods

--- src/main/java/com/example/hotels/repository/OrderRepository.java
+++ src/main/java/com/example/hotels/repository/OrderRepository.java
@@ -1,11 +1,16 @@
 package com.example.hotels.repository;
 
+import com.example.hotels.model.Hotel;
 import com.example.hotels.model.Order;
+import com.example.hotels.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
-
     Order findById(long id);
+    List<Order>findAllByHotelAndUser(Hotel hotel,User user);
+    List<Order> findByHotel(Hotel hotel);
 }
