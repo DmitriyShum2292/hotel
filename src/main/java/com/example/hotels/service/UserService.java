cd8fe47a71fb2f0530badc20b1d0fc77574ebb6e
Dmitriy <dmitriyshum2292@gmail.com>
2021-11-26 11:07:35 +0500

    new methods

--- src/main/java/com/example/hotels/service/UserService.java
+++ src/main/java/com/example/hotels/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.hotels.service;
 
+import com.example.hotels.dto.ResidentReponseDTO;
 import com.example.hotels.model.Order;
 import com.example.hotels.model.User;
 import org.springframework.stereotype.Component;
@@ -10,8 +11,10 @@ import java.util.List;
 public interface UserService {
 
     void save(User user);
+    User findById(long id);
     User findByNickName(String nickName);
     boolean citizenExist(User user) throws IOException;
+    ResidentReponseDTO getCitizenInfo(User user) throws IOException;
     User findCurrentUser();
     List<Order> findOrdersByUser();
     User findByUserId(long id);
