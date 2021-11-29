9a6cecfc89350ba39911bcad38860d30a3401778
Dmitriy <dmitriyshum2292@gmail.com>
2021-11-29 18:49:33 +0500

    swagger configuration

---
+++ src/main/java/com/example/hotels/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.example.hotels.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.hotels"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Hotel system swagger ui")
+                .description("\"Swagger configuration for application \"")
+                .version("1.1.1")
+                .contact(new Contact("Dmitriy", "http://ec2-3-123-229-208.eu-central-1.compute.amazonaws.com", "Dmitriy_Shumskiy@epam.com"))
+                .build();
+    }
+}
