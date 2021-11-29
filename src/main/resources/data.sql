f40e1692e109058945915f26bb8ba731d6d05b89
Dmitriy <dmitriyshum2292@gmail.com>
2021-11-29 11:30:53 +0500

    new data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
+CREATE TABLE IF NOT EXISTS external_api_credentials (
+    id INT,
+    key_id VARCHAR(255),
+    value VARCHAR(255),
+    primary key (id)
+);
 DELETE FROM external_api_credentials WHERE id=1;
 DELETE FROM external_api_credentials WHERE id=2;
 INSERT INTO external_api_credentials (id,key_id,value)
